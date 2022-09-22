26ceb80e3f08df236de5106c23c7f5a26ecf1129
albertustimothyy <albertustimothyy@gmail.com>
2022-09-22 22:19:45 +0700

    PT 2

---
+++ AlbertusTimothyGunawanJSleepKM/Account.java
@@ -0,0 +1,15 @@
+package AlbertusTimothyGunawanJSleepKM;
+
+
+/**
+ * Praktikum OOP
+ *
+ * @Albertus Timothy (
+ * @2106639472
+ */
+
+public class Account {
+    public String name;
+    public String email;
+    public String password;
+}
